eaa8860a9f7f4540b37ac2532012ae4d5a8f67e5
Jeffrey Johnston <rpgplayers.inc@gmail.com>
2024-07-29 21:48:49 +1000

    Include new examples.

--- src/main.rs
+++ src/main.rs
@@ -2,4 +2,9 @@ use utilprint::*;
 fn main() {
     utilprint("@x226He@x190ll@x155o @x119W@x120or@x084l@x085d, @x050t@x051his @x045is@x075 U@x069t@x105il@x099P@x135ri@x165nt@x201, let@x200's m@x205ake t@x204he@x210 w@x209or@x215ld co@x220lorf@x226ul!@w Here is a #2615, @ua Euro #20AC @Rs@ry@Rm@rb@Ro@rl@u, @Mand p@mu@Mr@mp@Ml@me @Mt@me@Mx@mt");
     print_help();
+    utilprint("I wonder what cool things we are going to make with UtilPrint?!".pastelbow());
+    utilprint("I really like rainbows, they are beautiful and awesome!".rainbow());
+    utilprint("Whatever it is, I hope it's something you love.".lover());
+    utilprint("This should be red!".red());
+    utilprint("I love to drink #2615 every single day, because it makes me #263A #2705".lover())
 }
